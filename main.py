4b3a6682b3551d5538f3eb1fdf2fa55ee3a73274
Stefan Nieuwenburg <nieuwenburg.stefan@gmail.com>
2022-01-07 18:06:18 +0100

    commit

--- main.py
+++ main.py
@@ -6,19 +6,19 @@ __human_name__ = "templates"
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/',methods=['GET'])
 def index(title = "index"):
     return render_template("index.html", title=title)
 
-@app.route("/home")
+@app.route("/home",methods=['GET'])
 def home():
     return redirect(url_for("index"))
 
-@app.route("/about")
+@app.route("/about",methods=['GET'])
 def about(title = "About"):
     return render_template("about.html", title=title)
 
-@app.route("/info")
+@app.route("/info",methods=['GET'])
 def info(title = "Info"):
     return render_template("info.html", title=title)
 
